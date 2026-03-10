ceacd786aab62b43d210304ff2be75c6101bf502
plaume8 <linus.sander@icloud.com>
2026-03-10 20:37:02 +0100

    feat: using reasoning xhigh

--- src/spoox/environment/model_clients/custom_client_openai.py
+++ src/spoox/environment/model_clients/custom_client_openai.py
@@ -102,7 +102,7 @@ class CustomOpenAIResponseAPIClient:
             model=self._model_id,
             input=parsed_messages,
             tools=parsed_tools,
-            reasoning={"effort": "high"},
+            reasoning={"effort": "xhigh"},
             store=True,
             max_output_tokens=100000,
         )
